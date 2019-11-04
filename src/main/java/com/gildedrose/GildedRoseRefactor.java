ee0dcea076528248a756ec526974ed1aed9c765c
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-11-04 13:23:25 +0100

    pom update

--- src/main/java/com/gildedrose/GildedRoseRefactor.java
+++ src/main/java/com/gildedrose/GildedRoseRefactor.java
@@ -1,11 +1,9 @@
 package com.gildedrose;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class GildedRose {
+public class GildedRoseRefactor {
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    public GildedRoseRefactor(Item[] items) {
         this.items = items;
     }
 
@@ -13,18 +11,68 @@ public class GildedRose {
 
 
     public void updateQuality() {
-        Logger logger = LoggerFactory.getLogger(GildedRose.class);
+
+        for (int i = 0; i < items.length; i++) {
+
+            switch (items[i].name) {
+                case "Aged Brie":
+
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+
+                    if (items[i].sellIn > 10) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }else if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 3;
+                            }
+                    }else if (items[i].sellIn < 11) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 2;
+                        }
+                    }
+
+                    if (items[i].sellIn <= 0) {
+                            items[i].quality = 0;
+                    }
+
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    // code block
+                    break;
+
+                default:
+
+                    if (items[i].sellIn <= 0){
+                        if (items[i].quality - 2 >= 0 ){
+                            items[i].quality = items[i].quality - 2;
+                        }else{
+                            items[i].quality = 0;
+                        }
+                    }
+
+                    if ( (items[i].quality - 1) >= 0  ){
+                        items[i].quality = items[i].quality - 1;
+                    }
+
+                    // code block
+            }
+        }
+/*
         for (int i = 0; i < items.length; i++) {
 
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
-
-                        logger.info("Test  : Hello World");
                     }
                 }
-            // soit brie or Backstage
+                // soit brie or Backstage
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
@@ -67,6 +115,8 @@ public class GildedRose {
                 }
             }
         }
+        */
+
     }
 
     private void internalUpdate(){
@@ -79,3 +129,4 @@ public class GildedRose {
         return items;
     }
 }
+
---
+++ src/test/java/com/gildedrose/GildedRoseRefactorTest.java
@@ -0,0 +1,152 @@
+package com.gildedrose;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+
+public class GildedRoseRefactorTest {
+
+    //Regular Names
+
+    @Test
+    void decrease_Regular_Name_Sellin_All_Day() {
+        Item[] items = new Item[] { new Item("foo", 10, 10) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].sellIn).isEqualTo(9);
+    }
+
+    @Test
+    void decrease_Regular_Name_Quality_All_Day() {
+        Item[] items = new Item[] { new Item("foo", 10, 10) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(9);
+    }
+
+    @Test
+    void date_Has_Passed_Quality_Degrade_Twice() {
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(8);
+    }
+
+    @Test
+    void date_Has_Passed_And_Quality_Never_Negativ() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(0);
+    }
+
+    @Test
+    void Quality_Never_Greater_Than_50() {
+        Item[] items = new Item[] { new Item("foo", 10,  50) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isLessThanOrEqualTo(50);
+    }
+
+
+
+    //Named Items
+
+    @Test
+    void sulfuras_Never_Change_Quality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(30);
+    }
+
+
+    @Test
+    void Quality_Never_Greater_Than_50_Backstage_Passes() {
+        Item[] items = new Item[] { new Item("Backstage passes", 10,  50) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isLessThanOrEqualTo(50);
+    }
+
+
+    @Test
+    void backstage_Increase_Quality() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 20,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(31);
+    }
+
+    @Test
+    void brie_Increase_Quality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 20,  10) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(11);
+    }
+
+
+    // 10 days less increase Value
+    @Test
+    void backstage_Increase_Quality_By_Two_While_Sellin_Under_Or_Less_10() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(32);
+    }
+
+    @Test
+    void brie_Not_Increase_Quality_By_Two_While_Sellin_Under_Or_Less_10() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(31);
+    }
+
+    @Test
+    void foo_Not_Increase_Quality_By_Two_While_Sellin_Under_Or_Less_10() {
+        Item[] items = new Item[] { new Item("foo", 10,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(29);
+    }
+
+
+    // 5 days less increase Value
+    @Test
+    void backstage_Increase_Quality_By_Three_While_Sellin_Under_Or_Less_5() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(33);
+    }
+
+    @Test
+    void brie_Not_Increase_Quality_By_Three_While_Sellin_Under_Or_Less_5() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(31);
+    }
+
+    @Test
+    void foo_Not_Increase_Quality_By_Three_While_Sellin_Under_Or_Less_5() {
+        Item[] items = new Item[] { new Item("foo", 5,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(29);
+    }
+
+
+    @Test
+    void backstage_Drop_Quality_To_zero_if_Sellin_equal_Or_Less_Zero() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0,  30) };
+        GildedRoseRefactor app = new GildedRoseRefactor(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(0);
+    }
+
+
+
+}
